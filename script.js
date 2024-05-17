e7ab8783fc0b05fcd077fa1c4c418c84f37c96bb
Milend4 <milenaleticia.15sa@gmail.com>
2024-05-17 16:15:08 -0300

    Atualizações  no html, css e js

--- script.js
+++ script.js
@@ -1,18 +1,84 @@
-// chamdando variaveis
+// chamando variáveis
 const progressBar = document.getElementById("progressBar");
 const buttonPlay = document.querySelector('#play');
 const buttonPause = document.querySelector('#pause');
 const tempoAtual = document.getElementById("tempoAtual");
 const tempoTotal = document.getElementById("tempoTotal");
-const buttonAvancar = document.getElementById("avancar");
-const buttonVoltar = document.getElementById("voltar");
+const buttonAvancar = document.getElementById('avancar');
+const buttonVoltar = document.getElementById('voltar');
 
-// metodo de audio
+const musicTitle = document.querySelector('.desc-musica p');
+const musicArtist = document.querySelector('.desc-musica span');
+const musicImage = document.querySelector('.conteudo-musica img');
+const cardPlayer = document.querySelector('.card-player');
 
-const music = new Audio('./assets/JamesMiller.mpeg');
+// vetor com as músicas e informações
+const playlist = [
+  {
+    title: "I lost myself in loving you",
+    artist: "James Miller",
+    audio: "./assets/JamesMiller.mpeg",
+    image: "./assets/JamesMiller.jpg",
+  },
+  {
+    title: "Lose control",
+    artist: "Teddy Swith",
+    audio: "./assets/LoseControl.mpeg",
+    image: "./assets/Teddy.jpg",
+  },
+  {
+    title: "Melhor só",
+    artist: "Baco Exu do Blues, KayBlack e Marquinho no Beat",
+    audio: "./assets/Kayblack.mpeg",
+    image: "./assets/MelhorSo.jpg",
+  },
+  {
+    title: "Ela",
+    artist: "Sorriso Maroto, Ferrugem",
+    audio: "./assets/Ela.mpeg",
+    image: "./assets/SorrisoMaroto.jpg",
+  },
+  {
+    title: "Fútil",
+    artist: "Mc Chris",
+    audio: "./assets/McChris.mpeg",
+    image: "./assets/futil.jpg",
+  }
+];
+
+const music = new Audio();
 let interval;
+let currentIndex = 0;
 
-//funçoes
+// funções
+function carregarMusica(currentIndex) {
+  const musica = playlist[currentIndex];
+  musicTitle.textContent = musica.title;
+  musicArtist.textContent = musica.artist;
+  musicImage.src = musica.image;
+  music.src = musica.audio;
+  
+  music.addEventListener('loadedmetadata', function() {
+    tempoTotal.textContent = formatarTempo(music.duration);
+  });
+  
+  tempoAtual.textContent = "00:00";
+  progressBar.value = 0;
+
+  cardPlayer.classList.remove('ILost', 'Lose', 'MelhorSo', 'Ela', 'Futil');
+
+  if (currentIndex === 0) {
+    cardPlayer.classList.add('ILost');
+  } else if (currentIndex === 1) {
+    cardPlayer.classList.add('Lose');
+  } else if (currentIndex === 2) {
+    cardPlayer.classList.add('MelhorSo');
+  }  else if (currentIndex === 3) {
+    cardPlayer.classList.add('Ela');
+  } else if (currentIndex === 4) {
+    cardPlayer.classList.add('Futil');
+  }
+}
 
 function formatarTempo(segundos) {
   const min = Math.floor(segundos / 60);
@@ -26,23 +92,39 @@ function updateMusicTime() {
   tempoAtual.textContent = formatarTempo(music.currentTime);
 }
 
-music.addEventListener('loadedmetadata', function () {
-  tempoTotal.textContent = formatarTempo(music.duration);
-});
-
+function avancar() {
+  currentIndex = (currentIndex + 1) % playlist.length; 
+  carregarMusica(currentIndex);
+  play();
+}
+  
 function play() {
-  buttonPlay.classList.add('hide');
-  buttonPause.classList.remove('hide');
+  buttonPlay.classList.toggle('hide');
+  buttonPause.classList.toggle('hide');
   music.play();
   interval = setInterval(updateMusicTime, 1000);
 }
 
 function pause() {
-  buttonPlay.classList.remove('hide');
-  buttonPause.classList.add('hide');
+  buttonPlay.classList.toggle('hide');
+  buttonPause.classList.toggle('hide');
   music.pause();
+  clearInterval(interval);
+}
+
+function voltar() {
+  currentIndex = (currentIndex - 1 + playlist.length) % playlist.length;
+  carregarMusica(currentIndex);
+  play();
 }
 
 
+carregarMusica(currentIndex); 
 buttonPlay.addEventListener('click', play);
 buttonPause.addEventListener('click', pause);
+buttonAvancar.addEventListener('click', avancar);
+buttonVoltar.addEventListener('click', voltar);
+progressBar.addEventListener('input', setProgress);
+
+
+
